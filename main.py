b80a8d2ca5128ddb52c6efa7b0b0900898451187
db2257 <97189710+db2257@users.noreply.github.com>
2024-01-03 17:17:58 +0200

    Brute force length<=3, else search in rockyou.txt

--- main.py
+++ main.py
@@ -1,61 +1,71 @@
-import zipfile
-import itertools
-from tqdm import tqdm
 import os
 import time
+import zipfile
+
+from tqdm import tqdm
 
 
 def brute_force_zip_password(zip_file_path, max_length=10):
     characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-    combinations_file = 'combinations.txt'
+    password_found = False
+    extracted_file_name = 'textulet.txt'
+    extracted_file_path = os.path.join(os.getcwd(), extracted_file_name)
 
     with zipfile.ZipFile(zip_file_path, 'r') as archive:
-        for length in range(1, max_length + 1):
+        for length in range(1, 4):
             start_time = time.time()
-            combinations = generate_combinations(characters, length, combinations_file)
+            combinations = generate_bruteforce_passwords(characters, length)
             elapsed_time = time.time() - start_time
             print(f"Time to generate combinations for length {length}: {elapsed_time:.2f} seconds")
-
+            print("Trying combinations of size:", length)
+            if length >= 3:
+                combinations = tqdm(combinations, desc="Trying combinations")
             for combination in combinations:
-                password = ''.join(combination)
                 try:
-                    archive.extractall(pwd=password.encode('utf-8'))
-                    print(f"Parola gasita: {password}")
-                    return password
-                except RuntimeError:
+                    archive.extract(extracted_file_name, pwd=combination.encode('utf-8'))
+                    print(f"Parola gasita: {combination}")
+                    password_found = True
+                    break
+                except:
                     pass
 
-    print("Nu s-a gasit nicio parola.")
-    return None
+        # If the password is not found, try passwords from rockyou.txt
+        if not password_found:
+            with open(r'C:\Users\Dragos\Desktop\rockyou.txt', 'r', encoding='latin-1') as rockyou_file:
+                for password in tqdm(rockyou_file, desc="Trying passwords from rockyou.txt"):
+                    password = password.strip()
+                    if len(password) <= max_length:
+                        try:
+                            archive.extract(extracted_file_name, pwd=password.encode('utf-8'))
+                            print(f"Password found in rockyou.txt: {password}")
+                            password_found = True
+                            break
+                        except:
+                            pass
 
-
-def generate_combinations(characters, length, combinations_file):
-    if length == 1:
-        result = [char for char in characters]
+    if not password_found:
+        print("Nu s-a gasit nicio parola.")
+    if os.path.exists(extracted_file_path):
+        os.remove(extracted_file_path)
+        print(f"Deleted {extracted_file_path} after extraction.")
     else:
-        prev_combinations = read_combinations_file(combinations_file, length - 1)
-        result = [char + prev_char for char in characters for prev_char in prev_combinations]
-
-    write_combinations_file(combinations_file, length, result)
-    print(result)
-    return result
+        print(f"{extracted_file_path} not found after extraction.")
 
 
-def read_combinations_file(combinations_file, length):
-    if os.path.exists(combinations_file):
-        with open(combinations_file, 'r') as file:
-            lines = file.readlines()
-            return [line.strip() for line in lines if len(line.strip()) == length]
-    return []
+def generate_bruteforce_passwords(characters, length, current_password=""):
+    if length == 0:
+        return [current_password]
 
+    passwords = []
+    for char in characters:
+        passwords.extend(generate_bruteforce_passwords(characters, length - 1, current_password + char))
 
-def write_combinations_file(combinations_file, length, combinations):
-    with open(combinations_file, 'w') as file:
-        for combination in combinations:
-            file.write(combination + '\n')
+    return passwords
 
 
 if __name__ == '__main__':
-    zip_file_path = 'C:/Users/Dragos/Desktop/lucrupython/ArchiveCracker/Archive.zip'
+    zip_file_path = r"C:\Users\Dragos\Desktop\lucrupython\ArchiveCracker\Archive.zip"
     brute_force_zip_password(zip_file_path)
-    # 8,5e17 combinatii incercate
+    #fac cu threading
+    #fac o functie care genereaza in fct de prima litera
+    #hashcat
