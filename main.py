b858296ee0342e27a402bb3f27a186fcebcee519
db2257 <97189710+db2257@users.noreply.github.com>
2024-01-07 11:26:47 +0200

    Updated the password testing method.
    Now it does not depend on the content of the Archive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import time
 import zipfile
 
@@ -8,48 +9,55 @@ from tqdm import tqdm
 def brute_force_zip_password(zip_file_path, max_length=10):
     characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     password_found = False
-    extracted_file_name = 'textulet.txt'
-    extracted_file_path = os.path.join(os.getcwd(), extracted_file_name)
 
+    zip_file_dir = os.path.dirname(zip_file_path)
+    auxiliary_folder_path = os.path.join(zip_file_dir, "auxiliary")
+    if not os.path.exists(auxiliary_folder_path):
+        os.makedirs(auxiliary_folder_path)
+
+    threshold = 4
     with zipfile.ZipFile(zip_file_path, 'r') as archive:
-        for length in range(1, 4):
+        for length in range(1, threshold):
             start_time = time.time()
             combinations = generate_bruteforce_passwords(characters, length)
             elapsed_time = time.time() - start_time
             print(f"Time to generate combinations for length {length}: {elapsed_time:.2f} seconds")
             print("Trying combinations of size:", length)
+
             if length >= 3:
                 combinations = tqdm(combinations, desc="Trying combinations")
             for combination in combinations:
                 try:
-                    archive.extract(extracted_file_name, pwd=combination.encode('utf-8'))
+                    archive.extractall(pwd=combination.encode('utf-8'), path=auxiliary_folder_path)
                     print(f"Parola gasita: {combination}")
+                    if os.path.exists(auxiliary_folder_path):
+                        shutil.rmtree(auxiliary_folder_path)
                     password_found = True
-                    break
+                    return combination
                 except:
                     pass
 
         # If the password is not found, try passwords from rockyou.txt
         if not password_found:
-            with open(r'C:\Users\Dragos\Desktop\rockyou.txt', 'r', encoding='latin-1') as rockyou_file:
+            with open(r'C:\Users\Dragos\Desktop\rockyou.txt', 'r', encoding='utf-8') as rockyou_file:
                 for password in tqdm(rockyou_file, desc="Trying passwords from rockyou.txt"):
                     password = password.strip()
                     if len(password) <= max_length:
                         try:
-                            archive.extract(extracted_file_name, pwd=password.encode('utf-8'))
-                            print(f"Password found in rockyou.txt: {password}")
+                            archive.extractall(pwd=password.encode('utf-8'), path=auxiliary_folder_path)
+                            print(f"Parola gasita: {password}")
+                            if os.path.exists(auxiliary_folder_path):
+                                shutil.rmtree(auxiliary_folder_path)
                             password_found = True
-                            break
+                            return password
                         except:
                             pass
 
     if not password_found:
         print("Nu s-a gasit nicio parola.")
-    if os.path.exists(extracted_file_path):
-        os.remove(extracted_file_path)
-        print(f"Deleted {extracted_file_path} after extraction.")
-    else:
-        print(f"{extracted_file_path} not found after extraction.")
+        
+    if os.path.exists(auxiliary_folder_path):
+        shutil.rmtree(auxiliary_folder_path)
 
 
 def generate_bruteforce_passwords(characters, length, current_password=""):
@@ -66,6 +74,3 @@ def generate_bruteforce_passwords(characters, length, current_password=""):
 if __name__ == '__main__':
     zip_file_path = r"C:\Users\Dragos\Desktop\lucrupython\ArchiveCracker\Archive.zip"
     brute_force_zip_password(zip_file_path)
-    #fac cu threading
-    #fac o functie care genereaza in fct de prima litera
-    #hashcat
